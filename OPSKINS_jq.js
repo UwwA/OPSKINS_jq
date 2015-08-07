c7edaad3c5e25beb750ff057b7a77146be58d9ba
Anton Gärdälv <anton@gardalv.se>
2015-08-07 16:22:11 +0200

    Code update
    Removed debugging code, added comments

--- OPSKINS_jq.js
+++ OPSKINS_jq.js
@@ -1,31 +1,34 @@
 //FOR USE WITH OPSKINS.com
+//Example url for testing: https://opskins.com/index.php?search_item=****&min=500&max=715&grade=Grade&type=Type&sort=f&loc=shop_search
+
+//Creates an array of all suggested prices
 var x = [];
 x.length = 0;
 $("span.market-name").each(function(){
 	x.push($(this).text().replace(/\D/g, ""));
 });
 
-console.log(x.toString());
-
+//Creates an array of all actual prices
 var y = [];
 y.length = 0;
 $("div.item-amount").each(function(){
 	y.push($(this).text().replace(/\D/g, ""));
 });
-console.log(y.toString());
 
+//Stores the difference between each object in an array
 var diff = [];
 diff.length = 0;
 for(i = 0;i<x.length;i++)
 {
 	diff.push(x[i]-y[i]);
 }
-console.log(diff.toString());
 
+//Calculates and stores profit percentages in an array
 var perc = [];
 var col = "";
 for(i = 0;i<x.length;i++)
 {
+	//Avoid division by zero
 	if(diff[i] != "0"){
 		perc.push(diff[i]/x[i]);
 	}
@@ -33,10 +36,9 @@ for(i = 0;i<x.length;i++)
 	{
 		perc.push("0");
 	}
-	
-	console.log(i,diff[i],"/",x[i]," = ",perc[i]);
 }
 
+//Prints out the profit percentages to the user
 var s = 0;
 $("div.item-add").each(function(){
 	if(perc[s] < 0.1){
